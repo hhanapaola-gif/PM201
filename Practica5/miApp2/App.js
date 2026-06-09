6e8e38d36f20160180f8709a538ec0f681ffe313
hhanapaola <hhanapaola@gmail.com>
2026-06-08 23:04:12 -0600

    Práctica 6. Creación de Perfil.js

--- Practica5/miApp2/App.js
+++ Practica5/miApp2/App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Saludo } from './components/Saludo';
 import { Saludo2 } from './components/Saludo2';
+import { Perfil } from './components/Perfil';
 
 /* Zona 2: Main - Componentes. Todo lo que contiene la vista*/
 export default function App() {
@@ -15,6 +16,8 @@ export default function App() {
       <Saludo/>
       <Text>--------------------------------</Text>
       <Saludo2/>
+      <Text>--------------------------------</Text>
+      <Perfil/>
       <StatusBar style="auto" />
 
     </View>
---
+++ Practica5/miApp2/components/Perfil.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native'
+
+export const Perfil = () =>{
+    return(
+
+        <View>
+            <Text>Nombre: Ana Paola Hernández Huacuja</Text>
+            <Text>Carrera: Ingeniería en sistemas computacionales</Text>
+            <Text>Materia: Programación Móvil</Text>
+            <Text>Cuatrimestre: 9no cuatrimestre</Text>
+        </View>
+    )
+}
