0020ac2292e8182f1c7eb69c6e604385712e5a2b
hhanapaola <hhanapaola@gmail.com>
2026-06-08 21:53:53 -0600

    Práctica 6

--- Practica5/miApp2/App.js
+++ Practica5/miApp2/App.js
@@ -1,15 +1,27 @@
+/* Zona 1: Importación de archivos y componentes  */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Saludo } from './components/Saludo';
+import { Saludo2 } from './components/Saludo2';
 
+/* Zona 2: Main - Componentes. Todo lo que contiene la vista*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Image source={require('./assets/wave.png')}/>
+      <Text>Hola mundo React Native!</Text>
+      <Text>--------------------------------</Text>
+      <Saludo/>
+      <Text>--------------------------------</Text>
+      <Saludo2/>
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
+/* Zona 3: Main - Estilos y posicionamiento*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
